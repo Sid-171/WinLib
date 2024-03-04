70f7be2250587a01dfbbe6699f2455d56d9f7f95
Siddhi Shinde <shindesiddhi638@gmail.com>
2024-03-04 20:18:21 +0530

    First Commit

---
+++ src/main/java/com/library/LibraryManagement/entities/LibEntity.java
@@ -0,0 +1,4 @@
+package com.library.LibraryManagement.entities;
+
+public class LibEntity {
+}
---
+++ src/main/java/com/library/LibraryManagement/service/LibService.java
@@ -0,0 +1,7 @@
+package com.library.LibraryManagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LibService {
+}
