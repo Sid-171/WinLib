6df855f0a0ec45bc7322c37cfea77ff45628f3fb
shrutij0912 <117350376+shrutij0912@users.noreply.github.com>
2024-03-08 12:27:24 +0530

    Update LibController.java

--- src/main/java/com/library/LibraryManagement/controller/LibController.java
+++ src/main/java/com/library/LibraryManagement/controller/LibController.java
@@ -1,7 +1,42 @@
-package com.library.LibraryManagement.controller;
+package com.library.libraryManage.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.library.libraryManage.entity.Info;
+import com.library.libraryManage.repository.LibRepo;
+import com.library.libraryManage.service.LibInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class LibController {
+@RequestMapping("/lib")
+public class Lib {
+    @Autowired
+    LibRepo librepo;
+
+    @Autowired
+    private LibInfo libinfo;
+
+    @GetMapping("/getAllBooks")
+    public List<Info> getAllBooks(){
+        return libinfo.getAllBooks();
+    }
+
+    @PostMapping("/createBooks")
+    public Info createBooks(@RequestBody Info Book1){
+        return librepo.save(Book1);
+    }
+
+    @PostMapping("/updateBook")
+    public Info updateBook(@RequestBody Info Book1){
+        return libinfo.updateBook(Book1);
+    }
+
+    @DeleteMapping("/deleteBook")
+    public Info deleteBook(@RequestBody Info Book1){
+        return libinfo.deleteBook(Book1);
+    }
 }
